478d6035e174f0453f9f0707f086034328c54e4a
Juan Bertino <bertinojimenezmorales@gmail.com>
2025-07-30 10:24:30 -0600

    update

---
+++ Mouredev/JS fundamentos/Ejercicios/oepradores.js
@@ -0,0 +1,52 @@
+// Crea una variable para cada operación artimetica básica
+let suma = 5 + 3; // Suma
+let resta = 10 - 4; // Resta
+let multiplicacion = 5 * 5; // Multiplicación
+let division = 20 / 4; // División
+let modulo = 10 % 3; // Módulo  
+console.log("Módulo :" , modulo);
+
+
+// 2 Crea ua variable para cada tipo de operación de asignación que haga uno
+// de las variables utilizadas para las operaciones aritméticas
+suma += 2; // Suma asignación
+resta -= 1; // Resta asignación
+multiplicacion *= 2; // Multiplicación asignación
+division /= 2; // División asignación
+modulo %= 2; // Módulo asignación   
+
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
+console.log("Módulo:", modulo);
+
+
+
+
+// 3 Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+console.log(5 > 3); // Mayor que
+console.log(5 < 10); // Menor que
+console.log(5 >= 5); // Mayor o igual que
+console.log(5 <= 5); // Menor o igual que
+console.log(5 === 5); // Igualdad estricta
+
+// 4 Imprime 5 comparaciones falsas con diferentes operadores de comparación
+console.log(5 < 3); // Menor que
+console.log(5 > 10); // Mayor que
+console.log(5 !== 5); // Desigualdad estricta
+console.log(5 <= 3); // Menor o igual que
+console.log(5 >= 10); // Mayor o igual que  
+
+
+// imprime una comparaci´on con el operador ternario
+let numero = 3;
+let resultado = (numero > 5) ? "El número es mayor que 5" : "El número es menor o igual a 5";
+console.log(resultado);
+
+
+
+
+
+
+
